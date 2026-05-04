66b8389bfc10613c670944b03185844f64aa4452
DESKTOP-U096J61\dhaip <brookemmcburneym@gmail.com>
2026-05-04 10:18:44 +0600

    start practice advance typeScript type

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = 'kolim';
+const age = 67;
+let isPassed = true;
+// console.log(name, age, isPassed)
+let score = [45, 56, 34, 56, 98];
+let playear = ['dippo', 'kolim', 'bosundhora'];
+console.log(score, playear);
+const student = ['kuddus', 56, true];
+console.log(student);
+const show = () => {
+    console.log('kolim is going');
+};
+const names = (name) => {
+    return `${name}`;
+};
+console.log(names('kolim'));
+console.log(names('rahim'));
+show();
+//# sourceMappingURL=app.js.map
---
+++ dist/app2.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let student = {
+    name: 'kolim',
+    age: 67,
+    isEnrolled: true,
+    course: ['math', 'physics', 'chemistry'],
+    score: [45, 56, 34],
+    info: ['kolim', 67, true]
+};
+console.log(student);
+//# sourceMappingURL=app2.js.map
